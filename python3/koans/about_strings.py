8c1f1ff3286e4a57a7a14d080a4bb15979433c3b
binary13 <justinmneil@gmail.com>
2016-01-25 12:19:30 -0500

    Reaching toward enlightenment...

--- python3/koans/about_strings.py
+++ python3/koans/about_strings.py
@@ -48,12 +48,12 @@ It was the worst of times."
 Howdy,
 world!
 """
-        self.assertEqual(__, len(string))
+        self.assertEqual(15, len(string))
 
     def test_triple_quoted_strings_need_less_escaping(self):
         a = "Hello \"world\"."
         b = """Hello "world"."""
-        self.assertEqual(__, (a == b))
+        self.assertEqual(True, (a == b))
 
     def test_escaping_quotes_at_the_end_of_triple_quoted_string(self):
         string = """Hello "world\""""
